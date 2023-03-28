3ada13b1b7485e513dcb13b85048b3b63e3576db
Tianbo <tianbo@getjerry.com>
2023-03-27 17:23:54 -0700

    prepare for swift package manager

---
+++ Sources/CalendarControl.xcframework/ios-arm64_x86_64-simulator/CalendarControl.framework/Headers/CalendarControl.h
@@ -0,0 +1,18 @@
+//
+//  CalendarControl.h
+//  CalendarControl
+//
+//  Created by Tianbo Qiu on 3/27/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarControl.
+FOUNDATION_EXPORT double CalendarControlVersionNumber;
+
+//! Project version string for CalendarControl.
+FOUNDATION_EXPORT const unsigned char CalendarControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarControl/PublicHeader.h>
+
+
